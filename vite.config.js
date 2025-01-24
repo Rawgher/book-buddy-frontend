88fc72d1e8c93b0af1c6b1f6dd62181d54a0745c
Rawgher <rdpender3@gmail.com>
2025-01-24 10:33:16 -0800

    new test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/',
+  root: './src',
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
   plugins: [react()],
   test: {
     globals: true,
