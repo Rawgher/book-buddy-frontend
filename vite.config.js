33f897746dc1fff8ed9ed9bf0ca1b76b47f6b1fc
Rawgher <rdpender3@gmail.com>
2025-01-24 10:26:31 -0800

    possible fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/',
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom", 
     setupFiles: "./src/setupTests.js", 
   },
+  server: {
+    open: true,
+    hmr: true,
+  }
 })
