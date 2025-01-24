42acd6644e41931e3e79677483cf84bde6f7c721
Rawgher <rdpender3@gmail.com>
2025-01-24 10:50:34 -0800

    reverting

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
   test: {
     globals: true,
     environment: "jsdom", 
-    setupFiles: "./setupTests.js", 
+    setupFiles: "./src/setupTests.js", 
   },
 })
