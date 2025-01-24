2011b2756f3d70b419f2dd1e02805dcba2961388
Rawgher <rdpender3@gmail.com>
2025-01-24 10:45:32 -0800

    again

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom", 
-    setupFiles: "./src/setupTests.js", 
-  },
   root: 'src',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
-  }
+  },
+  test: {
+    globals: true,
+    environment: "jsdom", 
+    setupFiles: "./setupTests.js", 
+  },
 })
